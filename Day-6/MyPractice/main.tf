e1a6cd00f2e10774f3ccdedf6d5f3008a933aee3
duttaarjun79 <arjundutta79@gmail.com>
2024-07-22 07:41:51 +0000

    Update

---
+++ Day-6/MyPractice/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "ami" {
+  description = "value"
+}
+
+variable "instance_type" {
+  description = "value"
+  type = map(string)
+
+  default = {
+    "dev" = "t2.nano"
+    "stage" = "t2.micro"
+    "prod" = "t2.micro"
+  }
+}
+
+module "ec2_instance" {
+  source = "./modules/ec2_instance"
+  ami = var.ami
+  instance_type = lookup(var.instance_type, terraform.workspace, "t2.nano")
+}
